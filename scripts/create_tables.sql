4873c034bc60acdd328a4c6d536b37ec3a91ab39
rvegrc <rvegrc@gmail.com>
2024-12-30 12:43:01 +0300

    create tables from df

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -13,6 +13,46 @@ ENGINE = MergeTree()
 ORDER BY tuple();
 
 
+create DATABASE if not exists ycup COMMENT 'database for yandex cup';
+
+GRANT select ON ycup.* TO developer; -- rewrite errors
+
+SHOW GRANTS FOR developer;
+
+drop table if exists ycup.control;
+    
+CREATE TABLE ycup.control (
+    id UInt32 COMMENT 'id поездки'
+    ,`stamp_ns` UInt64 COMMENT 'время в наносекундах от начала сцены'
+    ,`acceleration_level` Int16 COMMENT 'интенсивность ускорения, 0 - нет ускорения, + - ускорение, - - торможение'
+    ,`steering` Float32 COMMENT 'угол поворота руля в градусах относительно центрального положения'
+    ) 
+    ENGINE = MergeTree()
+    ORDER BY tuple();
+
+
+
+drop table if exists ycup.localization;
+    
+CREATE TABLE ycup.localization (
+    id UInt32 COMMENT 'id поездки'
+    ,`stamp_ns` UInt64 COMMENT 'время в наносекундах от начала сцены'
+    ,`x` Float32 COMMENT 'координата x, на восток'
+    ,`y` Float32 COMMENT 'координата y, на север'
+    ,`z` Float32 COMMENT 'координата z, в небо'
+    ,`roll` Float32 COMMENT 'угол крена в радианах'
+    ,`pitch` Float32 COMMENT 'угол тангажа в радианах'
+    ,`yaw` Float32 COMMENT 'угол рыскания в радианах, считается относительно оси x в направлении оси y'
+    ) 
+    ENGINE = MergeTree()
+    ORDER BY tuple();
+
+
+
+
+
+
+
 
 show grants for developer  -- show grants for user;
 
