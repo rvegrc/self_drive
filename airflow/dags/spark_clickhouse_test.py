2bf30a9aa3c67ebcab6f94dd16fc25f273320753
rvegrc <rvegrc@gmail.com>
2025-01-15 20:40:35 +0300

    del unnecessary libs from spark

--- airflow/dags/spark_clickhouse_test.py
+++ airflow/dags/spark_clickhouse_test.py
@@ -40,23 +40,24 @@ cpu = 30*3
     doc_md=__doc__
 )
 def spark_clickhouse_test():
-    # packages = [
-    # "com.clickhouse.spark:clickhouse-spark-runtime-3.5_2.12:0.8.0"
-    # ,"com.clickhouse:clickhouse-jdbc:0.7.1-patch1"
-    # ,"com.clickhouse:clickhouse-http-client:0.7.1-patch1"
-    # ,"org.apache.httpcomponents.client5:httpclient5:5.3.1"
-    # # ,"ai.catboost:catboost-spark_3.5_2.12:1.2.7"
-    # # ,"com.microsoft.azure:synapseml_2.12:1.0.8" 
-    # ]
-    packages = ["com.clickhouse.spark:clickhouse-spark-runtime-3.5_2.12:0.8.0",
-    "com.clickhouse:clickhouse-jdbc:0.7.0",
-    "com.clickhouse:clickhouse-client:0.7.0",
-    "com.clickhouse:clickhouse-http-client:0.7.0",
-    "org.apache.httpcomponents.client5:httpclient5:5.3.1",
-    'org.apache.sedona:sedona-spark-3.5_2.12:1.7.0',
-    'org.datasyslab:geotools-wrapper:1.7.0-28.5',
-    'uk.co.gresearch.spark:spark-extension_2.12:2.11.0-3.4'
+    packages = [
+    "com.clickhouse.spark:clickhouse-spark-runtime-3.5_2.12:0.8.0"
+    ,"com.clickhouse:clickhouse-jdbc:0.7.1-patch1"
+    ,"com.clickhouse:clickhouse-client:0.7.1-patch1"
+    ,"com.clickhouse:clickhouse-http-client:0.7.1-patch1"
+    ,"org.apache.httpcomponents.client5:httpclient5:5.3.1"
+    # ,"ai.catboost:catboost-spark_3.5_2.12:1.2.7"
+    # ,"com.microsoft.azure:synapseml_2.12:1.0.8" 
     ]
+    # packages = ["com.clickhouse.spark:clickhouse-spark-runtime-3.5_2.12:0.8.0",
+    # "com.clickhouse:clickhouse-jdbc:0.7.0",
+    # "com.clickhouse:clickhouse-client:0.7.0",
+    # "com.clickhouse:clickhouse-http-client:0.7.0",
+    # "org.apache.httpcomponents.client5:httpclient5:5.3.1",
+    # 'org.apache.sedona:sedona-spark-3.5_2.12:1.7.0',
+    # 'org.datasyslab:geotools-wrapper:1.7.0-28.5',
+    # 'uk.co.gresearch.spark:spark-extension_2.12:2.11.0-3.4'
+    # ]
 
 
 
